40291efc8285858513100de3b1af3e9740af9d39
alkorotkovv <akorotkov95@mail.ru>
2024-06-29 13:00:41 +0300

    table

--- src/WebSocket/Websocket.js
+++ src/WebSocket/Websocket.js
@@ -1,3 +1,2 @@
 export const Websocket = new WebSocket(`ws://localhost:3650`);
-//export const Websocket = new WebSocket(`wss://nn-portalp.niaepnn.ru/service/wss`);
-//export const Websocket = new WebSocket(`wss://nn-portaldmzp.niaepnn.ru/service/wss`);
+//export const Websocket = new WebSocket(`wss://domen.ru/service/wss`);
